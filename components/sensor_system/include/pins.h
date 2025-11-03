9c2ab8d208761ff70817d2c4897e1e4f254accb4
Jay White <jay.v.white@att.net>
2025-11-03 15:49:53 -0600

    cleaned up the pin.h assignments

--- components/sensor_system/include/pins.h
+++ components/sensor_system/include/pins.h
@@ -1,8 +1,10 @@
 /*
- * pins.h - ESP32 GPIO Pin Definitions
+ * pins.h - ESP32-C6 Feather GPIO Pin Definitions
  *
- * This file contains all pin definitions for the ESP32 board.
- * Pin mappings are based on ESP32 GPIO numbers (not Arduino pins).
+ * This file contains all pin definitions for the Adafruit ESP32-C6 Feather board.
+ * Pin mappings are based on ESP32-C6 GPIO numbers (not Arduino pin numbers).
+ * 
+ * Reference: Adafruit ESP32-C6 Feather pinout documentation
  */
 
 #ifndef PINS_H
@@ -10,92 +12,152 @@
 
 #include <driver/gpio.h>
 
-// Analog pins (ADC channels - ESP32 GPIO numbers)
-#define PIN_A0   GPIO_NUM_1    // GPIO1  - ADC1_CH0
-#define PIN_A1   GPIO_NUM_2    // GPIO2  - ADC1_CH1
-#define PIN_A2   GPIO_NUM_3    // GPIO3  - ADC1_CH2
-#define PIN_A3   GPIO_NUM_4    // GPIO4  - ADC1_CH3
-#define PIN_A4   GPIO_NUM_5    // GPIO5  - ADC1_CH4
-#define PIN_A5   GPIO_NUM_6    // GPIO6  - ADC1_CH5
-#define PIN_A6   GPIO_NUM_7    // GPIO7  - ADC1_CH6
-#define PIN_A7   GPIO_NUM_8    // GPIO8  - ADC1_CH7
+// ============================================================================
+// Analog Pins (ADC channels - ESP32-C6 GPIO numbers)
+// Note: Arduino pin numbers differ from GPIO numbers on this board
+// Arduino "A0" = GPIO1, Arduino "A1" = GPIO4, etc.
+// ============================================================================
+#define PIN_A0   GPIO_NUM_1    // GPIO1  - ADC1_CH1 (Arduino "A0"/"1", CircuitPython "A0")
+#define PIN_A1   GPIO_NUM_4    // GPIO4  - ADC1_CH4 (Arduino "A1"/"4", CircuitPython "A1")
+#define PIN_A2   GPIO_NUM_6    // GPIO6  - ADC1_CH6 (Arduino "A2"/"6", CircuitPython "A2/IO6")
+#define PIN_A3   GPIO_NUM_5    // GPIO5  - ADC1_CH5 (Arduino "A3"/"5", CircuitPython "A3/IO5")
+#define PIN_A4   GPIO_NUM_3    // GPIO3  - ADC1_CH3 (Arduino "A4"/"3", CircuitPython "A4")
+#define PIN_A5   GPIO_NUM_2    // GPIO2  - ADC1_CH2 (Arduino "A5"/"2", CircuitPython "A5")
 
-// Digital pins (ESP32 GPIO numbers)
-#define PIN_D0   GPIO_NUM_44   // GPIO44 - UART RX
-#define PIN_D1   GPIO_NUM_43   // GPIO43 - UART TX
-#define PIN_D2   GPIO_NUM_5    // GPIO5  - Digital pin 2
-#define PIN_D3   GPIO_NUM_6    // GPIO6  - Digital pin 3
-#define PIN_D4   GPIO_NUM_7    // GPIO7  - Digital pin 4
-#define PIN_D5   GPIO_NUM_8    // GPIO8  - Digital pin 5
-#define PIN_D6   GPIO_NUM_9    // GPIO9  - Digital pin 6
-#define PIN_D7   GPIO_NUM_10   // GPIO10 - Digital pin 7
-#define PIN_D8   GPIO_NUM_17   // GPIO17 - Digital pin 8
-#define PIN_D9   GPIO_NUM_18   // GPIO18 - Digital pin 9
-#define PIN_D10  GPIO_NUM_21   // GPIO21 - SPI CS
-#define PIN_D11  GPIO_NUM_33   // GPIO33 - SPI COPI (MOSI)
-#define PIN_D12  GPIO_NUM_34   // GPIO34 - SPI CIPO (MISO)
-#define PIN_D13  GPIO_NUM_35   // GPIO35 - SPI SCK
+// Note: A2 (GPIO6) can also function as digital IO6
+// Note: A3 (GPIO5) can also function as digital IO5
+// Note: GPIO0 is available as digital IO0 (Arduino "0") but NOT as analog A0
 
-// Power and control pins
-#define PIN_VIN      GPIO_NUM_39   // VIN input
-#define PIN_VBUS     GPIO_NUM_39   // USB VBUS (same as VIN)
-#define PIN_3V3      GPIO_NUM_40   // 3.3V output
-#define PIN_GND      GPIO_NUM_0    // Ground (dummy)
-#define PIN_RESET    GPIO_NUM_41   // Reset button (internal)
-#define PIN_BOOT0    GPIO_NUM_42   // Boot button
-#define PIN_BOOT1    GPIO_NUM_2    // GPIO2 - Boot mode selection
+// ============================================================================
+// Digital I/O Pins (ESP32-C6 GPIO numbers)
+// Note: Arduino pin numbers shown in parentheses for reference
+// ============================================================================
+#define PIN_D0   GPIO_NUM_0    // GPIO0  - Digital IO0 (Arduino "0", CircuitPython "IO0", ADC1_CH0)
+#define PIN_D1   GPIO_NUM_1    // GPIO1  - Digital IO1 (Arduino "1", CircuitPython "A0", ADC1_CH1)
+#define PIN_D2   GPIO_NUM_2    // GPIO2  - Digital IO2 (Arduino "2", CircuitPython "A5", ADC1_CH2)
+#define PIN_D3   GPIO_NUM_3    // GPIO3  - Digital IO3 (Arduino "3", CircuitPython "A4", ADC1_CH3)
+#define PIN_D4   GPIO_NUM_4    // GPIO4  - Digital IO4 (Arduino "4", CircuitPython "A1", ADC1_CH4)
+#define PIN_D5   GPIO_NUM_5    // GPIO5  - Digital IO5 (Arduino "5", CircuitPython "A3/IO5", ADC1_CH5)
+#define PIN_D6   GPIO_NUM_6    // GPIO6  - Digital IO6 (Arduino "6", CircuitPython "A2/IO6", ADC1_CH6)
+#define PIN_D7   GPIO_NUM_7    // GPIO7  - Digital IO7 (Arduino "7", CircuitPython "IO7")
+#define PIN_D8   GPIO_NUM_8    // GPIO8  - Digital IO8 (Arduino "8", CircuitPython "IO8")
+#define PIN_D9   GPIO_NUM_9    // GPIO9  - Digital IO9 (Arduino "9", CircuitPython "IO9", NeoPixel, Boot button)
+#define PIN_D10  GPIO_NUM_10   // GPIO10 - Digital IO10 (Arduino "10", SPI CS)
+#define PIN_D11  GPIO_NUM_11   // GPIO11 - Digital IO11 (Arduino "11")
+#define PIN_D12  GPIO_NUM_12   // GPIO12 - Digital IO12 (Arduino "12", JTAG TDI)
+#define PIN_D13  GPIO_NUM_13   // GPIO13 - Digital IO13 (Arduino "13", JTAG TDO)
+#define PIN_D14  GPIO_NUM_14   // GPIO14 - Digital IO14 (Arduino "14", CircuitPython "IO14", JTAG TCK)
+#define PIN_D15  GPIO_NUM_15   // GPIO15 - Digital IO15 (Arduino "15", CircuitPython "IO15", LED, JTAG TMS)
+#define PIN_D16  GPIO_NUM_16   // GPIO16 - Digital IO16 (Arduino "16", CircuitPython "TX", UART TX)
+#define PIN_D17  GPIO_NUM_17   // GPIO17 - Digital IO17 (Arduino "17", CircuitPython "RX", UART RX)
+#define PIN_D18  GPIO_NUM_18   // GPIO18 - Digital IO18 (Arduino "18", CircuitPython "SCL", I2C SCL)
+#define PIN_D19  GPIO_NUM_19   // GPIO19 - Digital IO19 (Arduino "19", CircuitPython "SDA", I2C SDA)
+#define PIN_D20  GPIO_NUM_20   // GPIO20 - Digital IO20 (EN pin, NEOPIXEL_I2C_POWER, SDIO_DATA0)
+#define PIN_D21  GPIO_NUM_21   // GPIO21 - Digital IO21 (Arduino "21", CircuitPython "SCK", SPI SCK)
+#define PIN_D22  GPIO_NUM_22   // GPIO22 - Digital IO22 (Arduino "22", CircuitPython "MOSI", SPI MOSI)
+#define PIN_D23  GPIO_NUM_23   // GPIO23 - Digital IO23 (Arduino "23", CircuitPython "MISO", SPI MISO)
 
-// Onboard LED pin (Adafruit Feather ESP32)
-#define PIN_ONBOARD_LED  GPIO_NUM_15  // GPIO15 - Onboard red LED
+// ============================================================================
+// Power and Control Pins
+// ============================================================================
+#define PIN_EN       GPIO_NUM_20  // GPIO20 - 3.3V regulator enable (also I2C power for STEMMA QT)
+#define PIN_3V3     0            // Not a GPIO - 3.3V output (hardware pin)
+#define PIN_GND     0            // Not a GPIO - Ground (hardware pin)
+#define PIN_VIN      0            // Not a GPIO - VIN input (hardware pin)
+#define PIN_VBUS     0            // Not a GPIO - USB VBUS (hardware pin)
+#define PIN_RESET    0            // Not a GPIO - Reset button (hardware)
+#define PIN_BOOT0    GPIO_NUM_9   // GPIO9 - Boot button (shared with NeoPixel/D4)
 
-// I2C bus pins (ESP32 default)
-#define PIN_I2C_SDA  GPIO_NUM_21   // GPIO21 - Default I2C SDA
-#define PIN_I2C_SCL  GPIO_NUM_22   // GPIO22 - Default I2C SCL
+// ============================================================================
+// Onboard LED and NeoPixel
+// ============================================================================
+#define PIN_ONBOARD_LED   GPIO_NUM_15  // GPIO15 - Onboard red LED
+#define PIN_NEOPIXEL      GPIO_NUM_9   // GPIO9 - Onboard NeoPixel (shared with Boot button)
 
-// SPI bus pins (ESP32 default)
-#define PIN_SPI_MOSI GPIO_NUM_23   // GPIO23 - Default SPI MOSI
-#define PIN_SPI_MISO GPIO_NUM_19   // GPIO19 - Default SPI MISO
-#define PIN_SPI_SCK  GPIO_NUM_18   // GPIO18 - Default SPI SCK
-#define PIN_SPI_CS   GPIO_NUM_5    // GPIO5  - Default SPI CS
+// ============================================================================
+// I2C Bus Pins (ESP32-C6 Feather default)
+// ============================================================================
+#define PIN_I2C_SDA  GPIO_NUM_19  // GPIO19 - I2C SDA (Data line)
+#define PIN_I2C_SCL  GPIO_NUM_18  // GPIO18 - I2C SCL (Clock line)
+// Note: GPIO20 (EN pin) must be pulled high to power STEMMA QT connector
 
-// UART pins (ESP32 default)
-#define PIN_SERIAL_RX GPIO_NUM_3   // GPIO3  - Default UART RX
-#define PIN_SERIAL_TX GPIO_NUM_1   // GPIO1  - Default UART TX
+// ============================================================================
+// SPI Bus Pins (ESP32-C6 Feather default)
+// ============================================================================
+#define PIN_SPI_SCK  GPIO_NUM_21  // GPIO21 - SPI Clock (SCK)
+#define PIN_SPI_MOSI GPIO_NUM_22  // GPIO22 - SPI MOSI (COPI - Controller Out/Peripheral In)
+#define PIN_SPI_MISO GPIO_NUM_23  // GPIO23 - SPI MISO (CIPO - Controller In/Peripheral Out)
+#define PIN_SPI_CS   GPIO_NUM_10  // GPIO10 - SPI Chip Select (CS) - default
 
-// Legacy sensor pin definitions (for compatibility - mapped to ESP32 GPIOs)
-#define FLOWSENSOR    PIN_D2    // Flow sensor interrupt pin
-#define TEMPSENSOR    PIN_D3    // One-wire temperature sensor
-#define DISCINPUT1    PIN_D4    // Discrete input 1
-#define DISCINPUT2    PIN_D5    // Discrete input 2
-#define CONFIGPIN1    PIN_D6    // Configuration pin 1
-#define CONFIGPIN2    PIN_D7    // Configuration pin 2
-#define CONFIGPIN3    PIN_D8    // Configuration pin 3
+// ============================================================================
+// UART Pins (ESP32-C6 Feather default)
+// ============================================================================
+#define PIN_SERIAL_RX GPIO_NUM_17  // GPIO17 - UART RX (Receive)
+#define PIN_SERIAL_TX GPIO_NUM_16  // GPIO16 - UART TX (Transmit)
 
-// GPIO expander I2C addresses (from original design)
-#define GPIO_EXPANDER_ADDR  0x20
+// ============================================================================
+// JTAG Debug Pins (for ESP-PROG connection)
+// ============================================================================
+#define PIN_JTAG_TDI GPIO_NUM_12  // GPIO12 - JTAG Data In
+#define PIN_JTAG_TDO GPIO_NUM_13  // GPIO13 - JTAG Data Out
+#define PIN_JTAG_TCK GPIO_NUM_14  // GPIO14 - JTAG Clock
+#define PIN_JTAG_TMS GPIO_NUM_15  // GPIO15 - JTAG Mode Select (shared with red LED!)
 
-// ADC I2C addresses (from original design)
+// ============================================================================
+// Legacy Sensor Pin Definitions (for compatibility)
+// Note: These use the corrected digital pin definitions
+// ============================================================================
+#define FLOWSENSOR    PIN_D7    // Flow sensor interrupt pin (GPIO7)
+#define TEMPSENSOR    PIN_D8    // One-wire temperature sensor (GPIO8)
+#define DISCINPUT1    PIN_D9    // Discrete input 1 (GPIO9, shared with NeoPixel/Boot)
+#define DISCINPUT2    PIN_D10   // Discrete input 2 (GPIO10, shared with SPI CS)
+#define CONFIGPIN1    PIN_D11   // Configuration pin 1 (GPIO11)
+#define CONFIGPIN2    PIN_D12   // Configuration pin 2 (GPIO12, shared with JTAG TDI)
+#define CONFIGPIN3    PIN_D13   // Configuration pin 3 (GPIO13, shared with JTAG TDO)
+
+// ============================================================================
+// I2C Device Addresses (from original design)
+// ============================================================================
+#define GPIO_EXPANDER_ADDR  0x20  // MCP23x17 GPIO expander
 #define ADS1115_ADDR        0x48  // 16-bit ADC
 #define ADS1015_ADDR        0x49  // 12-bit ADC
-
-// Environmental sensor I2C addresses
-#define BME280_ADDR         0x77  // Atmospheric sensor
-
-// RTC I2C address
+#define BME280_ADDR         0x77  // Atmospheric sensor (also 0x76)
 #define RTC_ADDR            0x69  // RV1805 RTC
-
-// OpenLog I2C address
 #define OPENLOG_ADDR        0x2A  // OpenLog datalogger
-
-// Buzzer I2C address
 #define BUZZER_ADDR         0x34  // Qwiic Buzzer
 
-// Pin function validation macros
-#define IS_ANALOG_PIN(pin)  ((pin) >= GPIO_NUM_1 && (pin) <= GPIO_NUM_8)
-#define IS_DIGITAL_PIN(pin) ((pin) >= GPIO_NUM_0 && (pin) <= GPIO_NUM_39 && (pin) != GPIO_NUM_24 && (pin) != GPIO_NUM_28 && (pin) != GPIO_NUM_29 && (pin) != GPIO_NUM_30 && (pin) != GPIO_NUM_31)
+// ============================================================================
+// Pin Function Validation Macros
+// ============================================================================
+// ESP32-C6 has GPIO 0-31 available (but not all are usable)
+#define IS_ANALOG_PIN(pin)  ((pin) >= GPIO_NUM_0 && (pin) <= GPIO_NUM_5)
+#define IS_DIGITAL_PIN(pin) ((pin) >= GPIO_NUM_0 && (pin) <= GPIO_NUM_23 && \
+                             (pin) != GPIO_NUM_24 && (pin) != GPIO_NUM_28 && \
+                             (pin) != GPIO_NUM_29 && (pin) != GPIO_NUM_30 && \
+                             (pin) != GPIO_NUM_31)
 #define IS_I2C_PIN(pin)     ((pin) == PIN_I2C_SDA || (pin) == PIN_I2C_SCL)
 #define IS_SPI_PIN(pin)     ((pin) == PIN_SPI_MOSI || (pin) == PIN_SPI_MISO || \
                              (pin) == PIN_SPI_SCK || (pin) == PIN_SPI_CS)
 #define IS_UART_PIN(pin)    ((pin) == PIN_SERIAL_RX || (pin) == PIN_SERIAL_TX)
+#define IS_JTAG_PIN(pin)    ((pin) == PIN_JTAG_TDI || (pin) == PIN_JTAG_TDO || \
+                             (pin) == PIN_JTAG_TCK || (pin) == PIN_JTAG_TMS)
+
+// ============================================================================
+// Important Pin Sharing Notes
+// ============================================================================
+// - GPIO0: Available as digital IO0, also ADC1_CH0 (but not Arduino "A0")
+// - GPIO1: Arduino "A0" and "1", can be analog or digital
+// - GPIO2: Arduino "A5" and "2", can be analog or digital
+// - GPIO3: Arduino "A4" and "3", can be analog or digital
+// - GPIO4: Arduino "A1" and "4", can be analog or digital
+// - GPIO5: Arduino "A3" and "5", can be analog or digital
+// - GPIO6: Arduino "A2" and "6", can be analog or digital
+// - GPIO9: Shared between NeoPixel, Boot button, and Digital IO9
+// - GPIO10: Shared between SPI CS and Digital IO10
+// - GPIO12: Shared between JTAG TDI and Digital IO12
+// - GPIO13: Shared between JTAG TDO and Digital IO13
+// - GPIO14: Shared between JTAG TCK and Digital IO14
+// - GPIO15: Shared between red LED, JTAG TMS, and Digital IO15 (disable LED when debugging)
+// - GPIO20: EN pin - also powers STEMMA QT connector and NeoPixel when high (NEOPIXEL_I2C_POWER)
 
 #endif /* PINS_H */
